195e2262059e66c8053df41b2f55288892f9d659
azank1 <33azank33@gmail.com>
2025-02-17 19:07:19 +0500

    added ZK-circuit base to fuzzyeureka

---
+++ ZK_Circuit/PoT_cpp/PoT.cpp
@@ -0,0 +1,250 @@
+#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+#include "circom.hpp"
+#include "calcwit.hpp"
+void Sum_0_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
+void Sum_0_run(uint ctx_index,Circom_CalcWit* ctx);
+void ProofOfTraining_1_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather);
+void ProofOfTraining_1_run(uint ctx_index,Circom_CalcWit* ctx);
+Circom_TemplateFunction _functionTable[2] = { 
+Sum_0_run,
+ProofOfTraining_1_run };
+Circom_TemplateFunction _functionTableParallel[2] = { 
+NULL,
+NULL };
+uint get_main_input_signal_start() {return 2;}
+
+uint get_main_input_signal_no() {return 7;}
+
+uint get_total_signal_no() {return 18;}
+
+uint get_number_of_components() {return 2;}
+
+uint get_size_of_input_hashmap() {return 256;}
+
+uint get_size_of_witness() {return 14;}
+
+uint get_size_of_constants() {return 3;}
+
+uint get_size_of_io_map() {return 0;}
+
+uint get_size_of_bus_field_map() {return 0;}
+
+void release_memory_component(Circom_CalcWit* ctx, uint pos) {{
+
+if (pos != 0){{
+
+if(ctx->componentMemory[pos].subcomponents)
+delete []ctx->componentMemory[pos].subcomponents;
+
+if(ctx->componentMemory[pos].subcomponentsParallel)
+delete []ctx->componentMemory[pos].subcomponentsParallel;
+
+if(ctx->componentMemory[pos].outputIsSet)
+delete []ctx->componentMemory[pos].outputIsSet;
+
+if(ctx->componentMemory[pos].mutexes)
+delete []ctx->componentMemory[pos].mutexes;
+
+if(ctx->componentMemory[pos].cvs)
+delete []ctx->componentMemory[pos].cvs;
+
+if(ctx->componentMemory[pos].sbct)
+delete []ctx->componentMemory[pos].sbct;
+
+}}
+
+
+}}
+
+
+// function declarations
+// template declarations
+void Sum_0_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
+ctx->componentMemory[coffset].templateId = 0;
+ctx->componentMemory[coffset].templateName = "Sum";
+ctx->componentMemory[coffset].signalStart = soffset;
+ctx->componentMemory[coffset].inputCounter = 3;
+ctx->componentMemory[coffset].componentName = componentName;
+ctx->componentMemory[coffset].idFather = componentFather;
+ctx->componentMemory[coffset].subcomponents = new uint[0];
+}
+
+void Sum_0_run(uint ctx_index,Circom_CalcWit* ctx){
+FrElement* signalValues = ctx->signalValues;
+u64 mySignalStart = ctx->componentMemory[ctx_index].signalStart;
+std::string myTemplateName = ctx->componentMemory[ctx_index].templateName;
+std::string myComponentName = ctx->componentMemory[ctx_index].componentName;
+u64 myFather = ctx->componentMemory[ctx_index].idFather;
+u64 myId = ctx_index;
+u32* mySubcomponents = ctx->componentMemory[ctx_index].subcomponents;
+bool* mySubcomponentsParallel = ctx->componentMemory[ctx_index].subcomponentsParallel;
+FrElement* circuitConstants = ctx->circuitConstants;
+std::string* listOfTemplateMessages = ctx->listOfTemplateMessages;
+FrElement expaux[2];
+FrElement lvar[3];
+uint sub_component_aux;
+uint index_multiple_eq;
+int cmp_index_ref_load = -1;
+{
+PFrElement aux_dest = &lvar[0];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+{
+PFrElement aux_dest = &lvar[1];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
+}
+{
+PFrElement aux_dest = &lvar[2];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
+}
+Fr_lt(&expaux[0],&lvar[2],&circuitConstants[0]); // line circom 10
+while(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &lvar[1];
+// load src
+Fr_add(&expaux[0],&lvar[1],&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[2])) + 1)]); // line circom 11
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+{
+PFrElement aux_dest = &lvar[2];
+// load src
+Fr_add(&expaux[0],&lvar[2],&circuitConstants[2]); // line circom 10
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_lt(&expaux[0],&lvar[2],&circuitConstants[0]); // line circom 10
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 0];
+// load src
+// end load src
+Fr_copy(aux_dest,&lvar[1]);
+}
+for (uint i = 0; i < 0; i++){
+uint index_subc = ctx->componentMemory[ctx_index].subcomponents[i];
+if (index_subc != 0)release_memory_component(ctx,index_subc);
+}
+}
+
+void ProofOfTraining_1_create(uint soffset,uint coffset,Circom_CalcWit* ctx,std::string componentName,uint componentFather){
+ctx->componentMemory[coffset].templateId = 1;
+ctx->componentMemory[coffset].templateName = "ProofOfTraining";
+ctx->componentMemory[coffset].signalStart = soffset;
+ctx->componentMemory[coffset].inputCounter = 7;
+ctx->componentMemory[coffset].componentName = componentName;
+ctx->componentMemory[coffset].idFather = componentFather;
+ctx->componentMemory[coffset].subcomponents = new uint[1]{0};
+}
+
+void ProofOfTraining_1_run(uint ctx_index,Circom_CalcWit* ctx){
+FrElement* signalValues = ctx->signalValues;
+u64 mySignalStart = ctx->componentMemory[ctx_index].signalStart;
+std::string myTemplateName = ctx->componentMemory[ctx_index].templateName;
+std::string myComponentName = ctx->componentMemory[ctx_index].componentName;
+u64 myFather = ctx->componentMemory[ctx_index].idFather;
+u64 myId = ctx_index;
+u32* mySubcomponents = ctx->componentMemory[ctx_index].subcomponents;
+bool* mySubcomponentsParallel = ctx->componentMemory[ctx_index].subcomponentsParallel;
+FrElement* circuitConstants = ctx->circuitConstants;
+std::string* listOfTemplateMessages = ctx->listOfTemplateMessages;
+FrElement expaux[2];
+FrElement lvar[2];
+uint sub_component_aux;
+uint index_multiple_eq;
+int cmp_index_ref_load = -1;
+{
+PFrElement aux_dest = &lvar[0];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[0]);
+}
+{
+uint aux_create = 0;
+int aux_cmp_num = 0+ctx_index+1;
+uint csoffset = mySignalStart+13;
+for (uint i = 0; i < 1; i++) {
+std::string new_cmp_name = "sum_diff";
+Sum_0_create(csoffset,aux_cmp_num,ctx,new_cmp_name,myId);
+mySubcomponents[aux_create+i] = aux_cmp_num;
+csoffset += 4 ;
+aux_cmp_num += 1;
+}
+}
+{
+PFrElement aux_dest = &lvar[1];
+// load src
+// end load src
+Fr_copy(aux_dest,&circuitConstants[1]);
+}
+Fr_lt(&expaux[0],&lvar[1],&circuitConstants[0]); // line circom 26
+while(Fr_isTrue(&expaux[0])){
+{
+PFrElement aux_dest = &signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[1])) + 8)];
+// load src
+Fr_sub(&expaux[0],&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[1])) + 1)],&signalValues[mySignalStart + ((1 * Fr_toInt(&lvar[1])) + 4)]); // line circom 27
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+{
+PFrElement aux_dest = &lvar[1];
+// load src
+Fr_add(&expaux[0],&lvar[1],&circuitConstants[2]); // line circom 26
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+Fr_lt(&expaux[0],&lvar[1],&circuitConstants[0]); // line circom 26
+}
+{
+uint cmp_index_ref = 0;
+{
+PFrElement aux_dest = &ctx->signalValues[ctx->componentMemory[mySubcomponents[cmp_index_ref]].signalStart + 1];
+// load src
+// end load src
+Fr_copyn(aux_dest,&signalValues[mySignalStart + 8],3);
+}
+// need to run sub component
+ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter -= 3;
+assert(!(ctx->componentMemory[mySubcomponents[cmp_index_ref]].inputCounter));
+Sum_0_run(mySubcomponents[cmp_index_ref],ctx);
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 11];
+// load src
+cmp_index_ref_load = 0;
+Fr_sub(&expaux[0],&ctx->signalValues[ctx->componentMemory[mySubcomponents[0]].signalStart + 0],&signalValues[mySignalStart + 7]); // line circom 36
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 12];
+// load src
+Fr_mul(&expaux[0],&signalValues[mySignalStart + 11],&signalValues[mySignalStart + 11]); // line circom 40
+// end load src
+Fr_copy(aux_dest,&expaux[0]);
+}
+{
+PFrElement aux_dest = &signalValues[mySignalStart + 0];
+// load src
+// end load src
+Fr_copy(aux_dest,&signalValues[mySignalStart + 12]);
+}
+for (uint i = 0; i < 1; i++){
+uint index_subc = ctx->componentMemory[ctx_index].subcomponents[i];
+if (index_subc != 0)release_memory_component(ctx,index_subc);
+}
+}
+
+void run(Circom_CalcWit* ctx){
+ProofOfTraining_1_create(1,0,ctx,"main",0);
+ProofOfTraining_1_run(0,ctx);
+}
+
